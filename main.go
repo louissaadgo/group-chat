72499b14dd9d56ae3038fe9702e2c4a102984961
louissaadgo <louissaadgo@gmail.com>
2021-09-10 20:00:22 +0300

    initial project structure

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/websocket/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Use("/ws", func(c *fiber.Ctx) error {
+		if websocket.IsWebSocketUpgrade(c) {
+			c.Locals("allowed", true)
+			return c.Next()
+		}
+		return fiber.ErrUpgradeRequired
+	})
+
+	app.Listen(":3000")
+}
